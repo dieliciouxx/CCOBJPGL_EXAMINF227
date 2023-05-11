d5fd81d2c233ef0cd001d8564bfb2c730f310437
Ashley Isidoro <111574791+dieliciouxx@users.noreply.github.com>
2023-05-11 11:08:51 +0800

    Add files via upload

---
+++ Exam/Locations/Cubao.java
@@ -0,0 +1,10 @@
+public class Cubao implements Locations {
+    
+    int airFare = 100;
+
+@Override
+public void accept(Tourist tourist) {
+tourist.visit(this);
+
+}
+}
---
+++ Exam/Locations/Pasig.java
@@ -0,0 +1,10 @@
+public class Pasig implements Locations {
+    
+    int airFare = 150;
+
+@Override
+public void accept(Tourist tourist) {
+tourist.visit(this);
+
+}
+}
